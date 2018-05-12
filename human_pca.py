3cd9116343cc16f15d392c2050a548124350a57f
sudo-Whateverman <nick@smartgreen.co.il>
2018-05-12 15:32:08 +0300

    added requirements txt

--- human_pca.py
+++ human_pca.py
@@ -6,7 +6,7 @@ from image_handler import reconstruct_and_save, reconstruct_with_normalization
 if __name__ == '__main__':
     # Setup
     logging.basicConfig(filename='human_pca.log', level=logging.DEBUG)
-    rootdir = './faces94'
+    rootdir = './faces94/female/slbirc'
     logging.debug("working on dir: " + rootdir)
     scale = True
     r = 5. / 10  # scaling factor that doesn't make my computer explode
@@ -15,10 +15,10 @@ if __name__ == '__main__':
         logging.debug("and scale is: " + str(r))
 
     # test
-    # effective_dim = create_covariance_matrix(rootdir, scale, r)
+    effective_dim = create_covariance_matrix(rootdir, scale, r)
     n = 50
     m = 0
-    im = reconstruct_to_n_degrees(n, m, scaling=scale, scaling_factor=r)
+    im = reconstruct_to_n_degrees(effective_dim, m, scaling=scale, scaling_factor=r)
     reconstruct_with_normalization(im, scaling=scale, scaling_factor=r,
                          name_of_file=str(m) + "th_photo_from_" + str(n) + "_eigenfaces.jpeg")
 
